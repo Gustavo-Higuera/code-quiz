bf270504125f8342b337f316949ba6201d931114
Coding Padawan <gustavohiguera9715@gmail.com>
2022-07-11 18:33:22 -0700

    added validate function

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,23 @@ var startButton = document.getElementById("btn");
 var timeEl = document.getElementById("time-block");
 var startingContentEl = document.querySelector(".content-container");
 var quizEl = document.querySelector(".quiz");
-var scoreEl = document.getElementById("score-message");
+var highScoresEl = document.getElementById("high-score");
+var currentScoreEl = document.getElementById("current-score");
 var feedbackEl = document.querySelector(".feedback");
 var questionEl = document.getElementById("question");
+var answersEl = document.getElementById("answers");
+var answerButton1 = document.getElementById("answer-btn1");
+var answerButton2 = document.getElementById("answer-btn2");
+var answerButton3 = document.getElementById("answer-btn3");
+var answerButton4 = document.getElementById("answer-btn4");
 
+var timeLeft = 120;
+var playerScore = 0;
 
 var questions = [{
   question: "Commonly used data types DO NOT include:",
   options: ["Strings", "Booleans", "Alerts", "Numbers"],
-  answer: "Alert"
+  answer: "Alerts"
 },
 {
   question: "The condition in an if/else statement is enclosed with _____.",
@@ -28,12 +36,14 @@ var questions = [{
   answer: "console.log"
 }];
 
+// When button start quiz is selected, it will start the timer, and call startQuiz function
 startButton.addEventListener("click", function () {
-  // startTimer();
+  startTimer();
   startQuiz();
 });
 
-var timeLeft = 120;
+var questionIndex = 0;
+currentScoreEl.textContent = `Current Score: ${playerScore}`;
 
 function startTimer() {
   var timer = setInterval(() => {
@@ -48,20 +58,70 @@ function startTimer() {
 
 function startQuiz() {
   startingContentEl.classList.add("hidden");
+  highScoresEl.classList.add("hidden");
+  currentScoreEl.classList.remove("hidden");
 
-  questionEl.innerText = questions.question;
+  showQuestions();
+}
 
+function showQuestions() {
+  questionEl.innerText = questions[0].question;
+  answersEl.classList.remove("hidden");
+  answerButton1.classList.remove("hidden");
+  answerButton2.classList.remove("hidden");
+  answerButton3.classList.remove("hidden");
+  answerButton4.classList.remove("hidden");
+
+  answerButton1.innerText = questions[0].options[0];
+  answerButton2.innerText = questions[0].options[1];
+  answerButton3.innerText = questions[0].options[2];
+  answerButton4.innerText = questions[0].options[3];
+
+  validateAnswer();
 }
 
 // WHEN I answer a question
 // THEN I am presented with another question
-
-
-
-
-
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
+
+function validateAnswer() {
+  answerButton1.addEventListener("click", function (e) {
+    if (e.target.innerText === questions[0].answer) {
+      console.log("This is the correct answer");
+      playerScore += 10;
+    } else {
+      console.log("Wrong!");
+      timeLeft -= 10;
+    }
+  })
+  answerButton2.addEventListener("click", function (e) {
+    if (e.target.innerText === questions[0].answer) {
+      console.log("This is the correct answer");
+      playerScore += 10;
+    } else {
+      console.log("Wrong!");
+      timeLeft -= 10;
+    }
+  })
+  answerButton3.addEventListener("click", function (e) {
+    if (e.target.innerText === questions[0].answer) {
+      console.log("This is the correct answer");
+      playerScore += 10;
+    } else {
+      console.log("Wrong!");
+    }
+  })
+  answerButton4.addEventListener("click", function (e) {
+    if (e.target.innerText === questions[0].answer) {
+      console.log("This is the correct answer");
+      playerScore += 10;
+    } else {
+      console.log("Wrong!");
+      timeLeft -= 10;
+    }
+  })
+}
 
 
 
